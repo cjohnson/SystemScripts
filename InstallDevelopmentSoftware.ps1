72555485ae73a6eb3cb08dc56f7c435a3a364d6d
cjohnson <collin.g.johnson@gmail.com>
2023-08-03 21:57:54 -0400

    Add Basic IDEs/Editors

---
+++ InstallDevelopmentSoftware.ps1
@@ -0,0 +1,7 @@
+# IDEs/Editors
+
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Microsoft.VisualStudio.2022.BuildTools
+winget install -e --id Microsoft.VisualStudio.2022.Community
+winget install -e --id JetBrains.IntelliJIDEA.Community
+winget install -e --id Notepad++.Notepad++
